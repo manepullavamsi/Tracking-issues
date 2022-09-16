f44378ca2109850cf7563744a4fec510216c2bcc
manepullavamsi <manepullavamsi1998@gmail.com>
2022-09-16 12:31:09 +0530

    Intial commit

---
+++ src/test/java/com/experiment/experience/springbootfeatures/SpringBootFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experiment.experience.springbootfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
