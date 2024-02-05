40c140cd774f52b6652a8a7def66bbe76f6bd52c
manepullavamsi <42999666+manepullavamsi@users.noreply.github.com>
2024-02-05 21:58:11 +0530

    Added respective dependency

--- spring-playground/spring-playground/src/main/java/com/spring/Tracking/issues/springplayground/SpringPlaygroundApplication.java
+++ spring-playground/spring-playground/src/main/java/com/spring/Tracking/issues/springplayground/SpringPlaygroundApplication.java
@@ -2,6 +2,7 @@ package com.spring.Tracking.issues.springplayground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.spring.Tracking.issues.springplayground.cricularDependency")
